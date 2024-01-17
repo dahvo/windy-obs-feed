5d5593f1fddd3419f7af6e5b500bd2c19ed66ada
David Mark Awad <100693051+dahvo@users.noreply.github.com>
2024-01-16 20:56:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Your Windy API key
-    const windyKey = WINDY_API_KEY;
+    const windyKey = 'x1xLatrDTSn7Lyz3qv5N2nnSz2x5q38S';
 
     // Windy API initialization options
     const options = {
